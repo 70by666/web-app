ede2b01204d97f8b746725b503908809358965d8
70by666 <70bytriplesix666@gmail.com>
2023-08-25 09:45:24 +0300

    init

---
+++ views.py
@@ -0,0 +1,12 @@
+def index():
+    with open('/home/t0by666/Documents/main_repo/3_Python_web_development/'
+              '011_python/0xx_Web-приложение на низком уровне: сокеты и '
+              'роутинг/templates/index.html') as tmp:
+        return tmp.read()
+    
+
+def blog():
+    with open('/home/t0by666/Documents/main_repo/3_Python_web_development/'
+              '011_python/0xx_Web-приложение на низком уровне: сокеты и '
+              'роутинг/templates/blog.html') as tmp:
+        return tmp.read()
